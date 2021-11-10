400300e050623ad1c95fd4ae38be7ac8bd51cff1
unknown <pcharm563@gmail.com>
2021-11-10 17:32:36 +0100

    Models and MongoDb

--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`Server listening on localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
