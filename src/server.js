0b4a67438297d1d8f248f7be263b5f75badb4b12
unknown <pcharm563@gmail.com>
2021-11-12 12:56:07 +0100

    add login and sessions

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
-import globalRouter from "./routers/rootRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+
+//Create sessions and put to req.session
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
